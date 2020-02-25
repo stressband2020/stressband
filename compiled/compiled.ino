f15a0d95dff84675aca65499b039b87873e74dcc
stressband2020 <stressbandthesis@gmail.com>
2020-02-25 16:39:29 +0800

    comment serials

--- compiled/compiled.ino
+++ compiled/compiled.ino
@@ -1,17 +1,17 @@
 //--------------temp
 int val;
-int tempPin = A4; //temp pin
+int tempPin = 4; //temp pin
 
 //----------------BPM
 #define USE_ARDUINO_INTERRUPTS true
 #include <PulseSensorPlayground.h>
-const int PulseWirePin = A0;     //bpm pin  
+const int PulseWirePin = 0;     //bpm pin  
 const int bpmled = 3;          
 int Threshold = 550;
 PulseSensorPlayground pulseSensor;
 
 //-------------GSR
-const int GSR=A2; //gsr pin
+const int GSR=2; //gsr pin
 int gsrValue=0;
 int gsr_average=0;
 
@@ -22,6 +22,7 @@ void setup()
   pulseSensor.analogInput(PulseWirePin);   
   pulseSensor.blinkOnPulse(bpmled);    
   pulseSensor.setThreshold(Threshold);
+  pulseSensor.begin();
 }
 void loop()
 {
@@ -31,17 +32,17 @@ void loop()
   float cel = mv/10;
   float farh = (cel*9)/5 + 32;
   
-  Serial.print("TEMPRATURE = ");
-  Serial.print(cel);
-  Serial.print("*C");
-  Serial.println();
-  delay(1000);
+//  Serial.print("TEMPRATURE = ");
+//  Serial.print(cel);
+//  Serial.print("*C");
+//  Serial.println();
+//  delay(1000);
 
   //---------------bpm 
   int myBPM = pulseSensor.getBeatsPerMinute();
   if (pulseSensor.sawStartOfBeat()) {           
-    Serial.print("BPM: ");                      
-    Serial.println(myBPM);                   
+//    Serial.print("BPM: ");                      
+//    Serial.println(myBPM);                   
   }
   delay(20);
 
@@ -54,6 +55,8 @@ void loop()
       delay(5);
       }
    gsr_average = sum/10;
-   Serial.println(gsr_average);
+//   Serial.println(gsr_average);
+   
    Serial.println("{gsr:" + String(gsr_average) + ", bpm:" + String(myBPM) + ", temperature:" + String(cel) + "}");
+   delay(500);
 }
